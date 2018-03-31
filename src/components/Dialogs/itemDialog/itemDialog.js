38b6aec77cf5771f3ab7d125893bef2dabe954ce
Albina-G <erfolg1996@mail.ru>
2018-03-31 14:07:55 +0500

    rewiev

--- src/components/Dialogs/itemDialog/itemDialog.js
+++ src/components/Dialogs/itemDialog/itemDialog.js
@@ -11,7 +11,11 @@ export default class ItemDialog extends React.Component {
 
         return (
             <div className="dialog-list__item">
-                <img src="https://imagineacademy.microsoft.com/content/images/microsoft-img.png" alt="" className="dialog-list__dialog-image" />
+                <img 
+                    src="https://imagineacademy.microsoft.com/content/images/microsoft-img.png" 
+                    alt=""
+                    className="dialog-list__dialog-image"
+                />
                 <div className="dialog-list__dialog-name">{name}</div>
                 <div className="dialog-list__last-msg">{messages.find(msg => { return msg.id === messagesIds }).text}</div>
                 <div className="dialog-list__last-msg-date">{messages.find(msg => { return msg.id === messagesIds }).date}</div>
