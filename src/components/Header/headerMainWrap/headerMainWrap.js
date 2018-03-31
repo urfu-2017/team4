3bb99a195f0600c5a12e36d2753212c948100e5c
Albina-G <erfolg1996@mail.ru>
2018-03-31 11:46:59 +0500

    lint

--- src/components/Header/headerMainWrap/headerMainWrap.js
+++ src/components/Header/headerMainWrap/headerMainWrap.js
@@ -10,7 +10,7 @@ export default class HeaderMainWrap extends React.Component {
     render() {
         return (
             <div className="header__main-wrap">
-                <img className="header__user-image" src={userImage} />
+                <img className="header__user-image" src={userImage} alt=""/>
                 <div className="header__user-name">{mainData[0].user.name}</div>
             </div>
         );
