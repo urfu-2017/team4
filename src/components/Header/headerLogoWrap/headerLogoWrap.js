2938f1bb6aa3e5693bf2b248f534df1f7eefd660
Albina-G <erfolg1996@mail.ru>
2018-03-31 11:34:47 +0500

    Add mobs

--- src/components/Header/headerLogoWrap/headerLogoWrap.js
+++ src/components/Header/headerLogoWrap/headerLogoWrap.js
@@ -1,29 +1,40 @@
 'use surict';
 
 import React from 'react';
+import { observer } from 'mobx-react';
 
 import './headerLogoWrap.css';
 import mainData from '../index.json';
 import Menu from '../menu/menu';
+import MenuStore from '../../../domain/menu-store';
 
+@observer
 export default class HeaderLogoWrap extends React.Component {
-    state = { stateMenu: false }
 
-    show = () => this.setState({ stateMenu: true })
-    close = () => this.setState({ stateMenu: false })
+    toogle = () => {
+        MenuStore.toggleLeftPanel();
+    }
 
+    /*  eslint-disable */
     render() {
-        const { stateMenu } = this.state;
-
+        console.info(MenuStore.isShow);
         return (
-            <div className="header__logo-wrap" onClick={stateMenu ? this.close : this.show}>
-                {stateMenu
-                    ? (
-                        <React.Fragment>
-                            <div className="header__menu">✖</div>
-                            <Menu />
-                        </React.Fragment>
-                    ) : <div className="header__menu">☰</div>}
+            <div className="header__logo-wrap">
+                {!MenuStore.isShow ? (
+                    <div 
+                        onClick={this.toogle}
+                        className="header__menu"
+                        children={'☰'}
+                    />
+                ) : (
+                    <React.Fragment>
+                        <div
+                            className="header__menu"
+                            onClick={this.toogle}
+                        >✖</div>
+                        <Menu/>
+                    </React.Fragment>
+                )}
                 <div className="header__logo">{mainData[0].title}</div>
             </div>
         );
--- src/components/Header/menu/menu.js
+++ src/components/Header/menu/menu.js
@@ -1,18 +1,13 @@
-'use surict';
-
 import React from 'react';
-
 import './menu.css';
 
-export default class Menu extends React.Component {
-    render() {
-        return (
-            <nav className="menu">
-                <ul className="menu__list">
-                    <li><a className="menu__list-link" href="">Contacts</a></li>
-                    <li><a className="menu__list-link" href="">Settings</a></li>
-                </ul>
-            </nav>
-        );
-    }
-}
+const Menu = () => (
+    <nav className="menu">
+        <ul className="menu__list">
+            <li><a className="menu__list-link" href="">Contacts</a></li>
+            <li><a className="menu__list-link" href="">Settings</a></li>
+        </ul>
+    </nav>
+);
+
+export default Menu;
