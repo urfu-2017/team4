7fa2359cd23ffb336bced2711f13d4c6edcc90b0
Ilya Kulygin <ruserixsian@gmail.com>
2018-04-02 00:27:30 +0500

    Исправлены урлы для авторизации

--- api/utils/sessions-store.js
+++ api/utils/sessions-store.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const { EventEmitter } = require('events');
+const { Cookie, Session } = require('express-session');
 
 class SessionStore extends EventEmitter {
     /**
@@ -30,6 +31,21 @@ class SessionStore extends EventEmitter {
             .then(() => callback(null))
             .catch(error => callback(error));
     }
+
+    /* eslint-disable no-param-reassign */
+    createSession(req, session) {
+        const { expires, originalMaxAge } = session.cookie;
+        session.cookie = new Cookie(session.cookie);
+
+        if (typeof expires === 'string') {
+            session.cookie.expires = new Date(expires);
+        }
+
+        session.cookie.originalMaxAge = originalMaxAge;
+        req.session = new Session(req, session);
+        return req.session;
+    }
+    /* eslint-enable no-param-reassign */
 }
 
 module.exports = SessionStore;
