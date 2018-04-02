6f46043a320bf28c992a7f1003efb775f7821dc8
nosovandrew <andrewnosov@outlook.com>
2018-04-02 21:11:37 +0500

    Fixed

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,56 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Message from './message.js';
 import './index.css';
-
-function goodDate(time) {
-    const dt = new Date(time * 1000);
-    const hours = dt.getHours();
-    const minutes = `0${dt.getMinutes()}`;
-    const seconds = `0${dt.getSeconds()}`;
-
-    return `${hours}:${minutes.substr(-2)}:${seconds.substr(-2)}`;
-}
-
-const Message = ({ username, text, avatar, date }) => {
-    return (
-        <div className="message">
-            <img
-                className="message__avatar"
-                src={avatar}
-                alt="Аватар"
-                onClick={function () {
-                    // Профиль пользователя
-                }}
-                />
-            <div className="message__body">
-                <span
-                    className="message__username"
-                    onClick={function () {
-                        // Профиль пользователя
-                    }}
-                    >
-                    {username}
-                </span>
-                <span className="message__text">
-                    {text}
-                </span>
-            </div>
-            <div className="message__date">
-                <span className="message__date-value">
-                    {goodDate(date)}
-                </span>
-            </div>
-        </div>
-    );
-};
-
-Message.propTypes = {
-    username: PropTypes.string.isRequired,
-    text: PropTypes.string.isRequired,
-    avatar: PropTypes.string.isRequired,
-    date: PropTypes.number.isRequired
-};
 
 export default class Chat extends React.Component {
     render() {
---
+++ src/components/Chat/message.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './index.css';
+
+function goodDate(time) {
+    const dt = new Date(time * 1000);
+    const hours = dt.getHours();
+    const minutes = String(dt.getMinutes()).padStart(2, '0');
+    const seconds = String(dt.getSeconds()).padStart(2, '0');
+
+    return `${hours}:${minutes}:${seconds}`;
+}
+  
+  
+const Message = ({ username, text, avatar, date }) => {
+    return (
+        <div className="message">
+            <img
+                className="message__avatar"
+                src={avatar}
+                alt="Аватар"
+                onClick={function () {
+                    // Профиль пользователя
+                }}
+                />
+            <div className="message__body">
+                <span
+                    className="message__username"
+                    onClick={function () {
+                        // Профиль пользователя
+                    }}
+                    >
+                    {username}
+                </span>
+                <span className="message__text">
+                    {text}
+                </span>
+            </div>
+            <div className="message__date">
+                <span className="message__date-value">
+                    {goodDate(date)}
+                </span>
+            </div>
+        </div>
+    );
+};
+
+Message.propTypes = {
+    username: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    date: PropTypes.number.isRequired
+};
+
+module.exports = Message;
