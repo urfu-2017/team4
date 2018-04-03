da52346df1af435efb64ec35dba1abacea6e2e56
Andrew Nosov <andrewnosov@outlook.com>
2018-04-03 09:29:16 +0500

    Chat (#44)
    * Chat
    * Lint fix

---
+++ src/components/Chat/message.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './index.css';
+
+function goodDate(time) {
+    const dt = new Date(time * 1000);
+    const hours = dt.getHours();
+    const minutes = String(dt.getMinutes()).padStart(2, '0');
+    const seconds = String(dt.getSeconds()).padStart(2, '0');
+
+    return `${hours}:${minutes}:${seconds}`;
+}
+
+const Message = ({ username, text, avatar, date }) => (
+    <div className="message">
+        <img
+            className="message__avatar"
+            src={avatar}
+            alt="Аватар"
+        />
+        <div className="message__body">
+            <span className="message__username">
+                {username}
+            </span>
+            <span className="message__text">
+                {text}
+            </span>
+        </div>
+        <div className="message__date">
+            <span className="message__date-value">
+                {goodDate(date)}
+            </span>
+        </div>
+    </div>
+);
+
+Message.propTypes = {
+    username: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    date: PropTypes.number.isRequired
+};
+
+module.exports = Message;
