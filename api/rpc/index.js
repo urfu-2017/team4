d99d67f675b9d99c07af1a1d354871b61234303a
Az404 <ztbzak1@rambler.ru>
2018-04-08 22:09:51 +0500

    KG-19: Добавление в контакты и список контактов (#58)
    * Добавление в контакты и получение списка контактов через RPC
    
    * fix build
    
    * rpc fixes

--- api/rpc/index.js
+++ api/rpc/index.js
@@ -3,6 +3,8 @@
 const RPC = require('../utils/rpc');
 const fetchUser = require('./users/fetch-user');
 const fetchDialogs = require('./users/fetch-dialogs');
+const fetchContacts = require('./users/fetch-contacts');
+const addContact = require('./users/add-contact');
 
 const joinToDialogs = require('./dialogs/join-to-dialogs');
 const createDialog = require('./dialogs/create-dialog');
@@ -13,6 +15,8 @@ const fetchHistory = require('./dialogs/fetch-history');
 module.exports = () => {
     RPC.Registry.register('fetchUser', fetchUser);
     RPC.Registry.register('fetchDialogs', fetchDialogs);
+    RPC.Registry.register('fetchContacts', fetchContacts);
+    RPC.Registry.register('addContact', addContact);
 
     RPC.Registry.register('sendMessage', sendMessage);
     RPC.Registry.register('fetchHistory', fetchHistory);
