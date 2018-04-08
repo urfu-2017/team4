1e91b91d00e008ae9212164993bb9548e3b4850a
AZ404 <ztbzak1@rambler.ru>
2018-04-08 21:55:33 +0500

    rpc fixes

--- api/rpc/users/fetch-contacts.js
+++ api/rpc/users/fetch-contacts.js
@@ -3,6 +3,6 @@
 const UserManager = require('../../managers/users');
 
 module.exports = async (params, response) => {
-    const currentUsername = response.socket.handshake.session;
+    const currentUsername = response.socket.handshake.user.username;
     response.success(await UserManager.getContacts(currentUsername));
 };
