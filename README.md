25f159bb193c6e484aab124fb0d559f945eea3e7
Ilya Kulygin <ruserixsian@gmail.com>
2018-04-15 23:45:25 +0500

    KG-44: Удаление реализации бэкенда и перевод клиента на боевой сервер

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 [![Build Status](https://travis-ci.org/urfu-2017/team4.svg?branch=master)](https://travis-ci.org/urfu-2017/team4)
 # Kilogram Messenger
 
-Инструкция по запуску
+Запуск клиента `npm run client:start`
--- server/utils/rpc/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const Builder = require('jsonrpc-lite');
-const Response = require('./response');
-const Registry = require('./registry');
-
-module.exports = {
-    Builder, Response, Registry, Error: Builder.JsonRpcError
-};
