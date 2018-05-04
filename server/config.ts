0c99da19dcb2ae296bff83eb7d0325329608e659
Ilya Kulygin <ruserixsian@gmail.com>
2018-05-05 00:17:59 +0500

    DEPLOY: Сконфигурирован sequelize

--- server/config.ts
+++ server/config.ts
@@ -22,3 +22,11 @@ export const UPLOADS_SERVER_PATH = './static/uploads';
 export const UPLOADS_URL_PATH = '/uploads';
 
 export const WEATHER_API_TOKEN = process.env.WEATHER_API_TOKEN;
+
+export const POSTGRES = {
+    HOST: process.env.POSTGRES_HOST!,
+    PORT: process.env.POSTGRES_PORT!,
+    USERNAME: process.env.POSTGRES_USERNAME!,
+    PASSWORD: process.env.POSTGRES_PASSWORD!,
+    DATABASE: process.env.POSTGRES_DATABASE!
+};
