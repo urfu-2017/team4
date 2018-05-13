d20c682e0ca995870b0a1b9a9fd161dcb99794c9
San4eyZ <sanya.rogov37@gmail.com>
2018-05-13 21:46:56 +0500

    Почти готова тема

--- client/components/Header/index.tsx
+++ client/components/Header/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import b from 'b_';
+import { observer } from 'mobx-react';
 
-import './Header.css';
 import HeaderLogoWrap from './HeaderLogoWrap';
 import HeaderMainWrap from './HeaderMainWrap';
 
-const Header = () => (
-    <header className="header">
+import uiStore from '../../domain/ui-store';
+
+import './Header.css';
+
+const Header: React.SFC = observer(() => (
+    <header className={b('header', { dark: uiStore.isDark })}>
         <HeaderLogoWrap />
         <HeaderMainWrap />
     </header>
-);
+));
 
 export default Header;
