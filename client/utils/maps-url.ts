3615abe237ecb6ffdb2854fc3347fe9417d6427a
Ilya Kulygin <ruserixsian@gmail.com>
2018-05-14 01:13:08 +0500

    Ширина и долгота поменялись местами

--- client/utils/maps-url.ts
+++ client/utils/maps-url.ts
@@ -1,6 +1,6 @@
 const baseUrl = 'https://static-maps.yandex.ru/1.x/?size=650,450&l=map&z=14';
 
 export default function (lat: number, lon: number) {
-    const coords = [lat, lon].join(',');
+    const coords = [lon, lat].join(',');
     return `${baseUrl}&ll=${coords}&pt=${coords},round`;
 }
