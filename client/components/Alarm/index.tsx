13265bbd42b9190f4543f0c790698b76e8cd3547
San4eyZ <sanya.rogov37@gmail.com>
2018-05-19 14:14:34 +0500

    Ночная тема для timer

--- client/components/Alarm/index.tsx
+++ client/components/Alarm/index.tsx
@@ -3,59 +3,84 @@ import b_ from 'b_';
 import { observer } from 'mobx-react';
 
 import Button from '../Button';
+
 import deathtimerStore from '../../domain/deathtimer-store';
+import uiStore from '../../domain/ui-store';
 
 import './Alarm.css';
 
 const b = b_.with('alarm-picker');
 
-const Alarm = () => (
-    <div className={b()}>
-        <Button className={b('button-state')} onClick={deathtimerStore.toggleActive}>
-            {deathtimerStore.isActive ? 'OFF' : 'ON'}
-        </Button>
-        <div className={b('timer')}>
-            <div className={b('days')}>
-                <small>Часы</small>
-                <Button className={b('button-up')} onClick={deathtimerStore.changeTime('hours')}>
-                    +
-                </Button>
-                <input type="number" value={deathtimerStore.hours} disabled={true} />
-                <Button
-                    className={b('button-down')}
-                    onClick={deathtimerStore.changeTime('hours', false)}
-                >
-                    -
-                </Button>
-            </div>
-            <div className={b('hour')}>
-                <small>Минуты</small>
-                <Button className={b('button-up')} onClick={deathtimerStore.changeTime('minutes')}>
-                    +
-                </Button>
-                <input type="number" value={deathtimerStore.minutes} disabled={true} />
-                <Button
-                    className={b('button-down')}
-                    onClick={deathtimerStore.changeTime('minutes', false)}
-                >
-                    -
-                </Button>
-            </div>
-            <div className={b('minutes')}>
-                <small>Секунды</small>
-                <Button className={b('button-up')} onClick={deathtimerStore.changeTime('seconds')}>
-                    +
-                </Button>
-                <input type="number" value={deathtimerStore.seconds} disabled={true} />
-                <Button
-                    className={b('button-down')}
-                    onClick={deathtimerStore.changeTime('seconds', false)}
-                >
-                    -
-                </Button>
+const Alarm = () => {
+    const dark = uiStore.isDark;
+
+    return (
+        <div className={b({ dark })}>
+            <Button
+                className={b('button-state')}
+                onClick={deathtimerStore.toggleActive}
+                type={dark ? 'dark' : 'main'}
+            >
+                {deathtimerStore.isActive ? 'OFF' : 'ON'}
+            </Button>
+            <div className={b('timer')}>
+                <div className={b('days')}>
+                    <small>Часы</small>
+                    <Button
+                        className={b('button-up')}
+                        onClick={deathtimerStore.changeTime('hours')}
+                        type={dark ? 'dark' : 'main'}
+                    >
+                        +
+                    </Button>
+                    <input type="text" value={deathtimerStore.hours} disabled={true} />
+                    <Button
+                        className={b('button-down')}
+                        onClick={deathtimerStore.changeTime('hours', false)}
+                        type={dark ? 'dark' : 'main'}
+                    >
+                        -
+                    </Button>
+                </div>
+                <div className={b('hour')}>
+                    <small>Минуты</small>
+                    <Button
+                        className={b('button-up')}
+                        onClick={deathtimerStore.changeTime('minutes')}
+                        type={dark ? 'dark' : 'main'}
+                    >
+                        +
+                    </Button>
+                    <input type="text" value={deathtimerStore.minutes} disabled={true} />
+                    <Button
+                        className={b('button-down')}
+                        onClick={deathtimerStore.changeTime('minutes', false)}
+                        type={dark ? 'dark' : 'main'}
+                    >
+                        -
+                    </Button>
+                </div>
+                <div className={b('minutes')}>
+                    <small>Секунды</small>
+                    <Button
+                        className={b('button-up')}
+                        onClick={deathtimerStore.changeTime('seconds')}
+                        type={dark ? 'dark' : 'main'}
+                    >
+                        +
+                    </Button>
+                    <input type="text" value={deathtimerStore.seconds} disabled={true} />
+                    <Button
+                        className={b('button-down')}
+                        onClick={deathtimerStore.changeTime('seconds', false)}
+                        type={dark ? 'dark' : 'main'}
+                    >
+                        -
+                    </Button>
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default observer(Alarm);
